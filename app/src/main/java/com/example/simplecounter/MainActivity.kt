35a1bec238f43d5280464034fc2114de702d0dfd
HydraHYD <waynetian22@gmail.com>
2022-04-29 00:08:33 -0400

    update

--- app/src/main/java/com/example/simplecounter/MainActivity.kt
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -2,16 +2,34 @@ package com.example.simplecounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    public upButton Button
-    public downButton: Button
+    private lateinit var upButton : Button
+    private lateinit var downButton: Button
+    private lateinit var counterDisplay: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        upButton.setlis
+        var counter = 0
+
+        upButton = findViewById(R.id.up_counter)
+        downButton = findViewById(R.id.down_counter)
+        counterDisplay = findViewById(R.id.main_counter)
+
+        counterDisplay.setText("$counter")
+
+        upButton.setOnClickListener{
+            counter += 1
+            counterDisplay.setText("$counter")
+        }
+        downButton.setOnClickListener{
+            counter -= 1
+            counterDisplay.setText("$counter")
+        }
 
     }
 }
