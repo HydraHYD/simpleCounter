9b84c9cbaeb795806d9e524ed639e1d6ace2eb6b
HydraHYD <waynetian22@gmail.com>
2022-04-28 23:57:57 -0400

    Added files

---
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.simplecounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    public upButton Button
+    public downButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        upButton.setlis
+
+    }
+}
